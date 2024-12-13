bcc20211d6e8a15569428c8f31b2717aa1438d76
ERI <eri@cparta.se>
2024-12-13 11:40:02 +0100

    session bits

--- rinser/auth.go
+++ rinser/auth.go
@@ -1,12 +1,15 @@
 package rinser
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"regexp"
 
 	"github.com/linkdata/rinse/jwt"
 )
+
+var ErrNoJWTFoundInHeader = fmt.Errorf("no JWT found in header")
 
 func (rns *Rinse) AskForAuthFn(fn http.HandlerFunc) http.Handler {
 	return rns.JawsAuth.Wrap(http.HandlerFunc(fn))
@@ -18,12 +21,12 @@ func (rns *Rinse) AuthFn(fn http.HandlerFunc) http.Handler {
 	})
 }
 
+// Checks for JWT in header or session, if no valid JWT is found, redirects to login
 func (rns *Rinse) CheckAuth(w http.ResponseWriter, r *http.Request, fn http.HandlerFunc) {
-
 	/*
-	   - kolla om det finns JWT i header, använd den
-	   - om den inte finns, kolla om man kan hämta en från jaws sessionen
-	   - om inte det, redirecta till login så man kna hämta en
+		If no token is found in header, check whether there is a valid token in session
+		If token found but not valid, return error respose
+		If no token found in neither header nor session,
 	*/
 	var (
 		inHeader  bool
@@ -31,23 +34,16 @@ func (rns *Rinse) CheckAuth(w http.ResponseWriter, r *http.Request, fn http.Hand
 		err       error
 	)
 
-	/*
-		If no token is found in header, check whether there is a valid token in session
-		If token found but not valid, return error respose
-		If no token found in neither header nor session,
-	*/
 	token, err := GetJWTFromHeader(r)
 	if err == nil {
 		inHeader, err = jwt.VerifyJWT(token, rns.JWTPublicKeys)
-		slog.Warn("inHeader?", inHeader)
 		if err == nil {
-			// rns.JawsAuth.SessionTokenKey = token //TODO test this
+			rns.JawsAuth.SessionTokenKey = token
 		} else {
 			SendHTTPError(w, http.StatusBadRequest, err)
 		}
-	} else if err != jwt.ErrNoJWTFoundInHeader {
+	} else {
 		inSession, _ = rns.FoundValidJWTInSession()
-		slog.Warn("InSession?", inSession)
 	}
 
 	if inHeader || inSession {
@@ -55,9 +51,9 @@ func (rns *Rinse) CheckAuth(w http.ResponseWriter, r *http.Request, fn http.Hand
 		slog.Warn("[DEBUG] fn")
 	} else {
 		// TODO DEBUG/DEV
-		HTTPJSON(w, http.StatusTeapot, "REDIRECTING")
-		//new_fn := rns.JawsAuth.Wrap(http.HandlerFunc(fn))
-		//new_fn.ServeHTTP(w, r)
+		//HTTPJSON(w, http.StatusTeapot, "REDIRECTING")
+		new_fn := rns.JawsAuth.Wrap(http.HandlerFunc(fn))
+		new_fn.ServeHTTP(w, r)
 		slog.Warn("[DEBUG] new_fn")
 	}
 }
@@ -72,7 +68,7 @@ func GetJWTFromHeader(r *http.Request) (string, error) {
 	header := r.Header
 	auth := header.Get("Authorization")
 	if auth == "" {
-		return "", jwt.ErrNoJWTFoundInHeader
+		return "", ErrNoJWTFoundInHeader
 	}
 
 	re := regexp.MustCompile(`(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)`)
