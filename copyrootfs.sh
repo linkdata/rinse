24e611fce0b01882ceaf7174a58f7e8b31a414ac
Johan Lindh <johan@linkdata.se>
2024-12-18 12:00:05 +0100

    smarter copyrootfs

--- copyrootfs.sh
+++ copyrootfs.sh
@@ -6,3 +6,5 @@ fi
 wd=$(pwd)
 mkdir -p $wd/rootfs
 cp -rp $(podman image mount localhost/rinse)/* $wd/rootfs/
+podman image unmount localhost/rinse > /dev/null
+echo To remove rootfs: podman unshare rm -rf rootfs/
