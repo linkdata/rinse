c56476abddff5d632584d356bc6e412769b961b0
Johan Lindh <johan@linkdata.se>
2025-01-10 11:40:57 +0100

    wip

--- copyrootfs.sh
+++ copyrootfs.sh
@@ -3,8 +3,10 @@ if [ $(id -u) -ne 0 ]; then
   podman unshare $0
   exit
 fi
-wd=$(pwd)
-mkdir -p $wd/rootfs
-cp -rp $(podman image mount localhost/rinse)/* $wd/rootfs/
-podman image unmount localhost/rinse > /dev/null
-echo To remove rootfs: podman unshare rm -rf rootfs/
+IMAGE='localhost/rinse'
+[ -n "$1" ] && IMAGE=$1
+WORKDIR=$(pwd)
+mkdir -p $WORKDIR/rootfs
+cp -rp $(podman image mount $IMAGE)/* $WORKDIR/rootfs/
+podman image unmount $IMAGE > /dev/null
+echo To remove $WORKDIR/rootfs: podman unshare rm -rf $WORKDIR/rootfs/
