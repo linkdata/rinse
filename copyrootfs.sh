4c24c4f2c62fe4c1b098f4c2827e52308a293780
Johan Lindh <johan@linkdata.se>
2025-01-10 11:54:36 +0100

    wip

--- copyrootfs.sh
+++ copyrootfs.sh
@@ -1,12 +1,19 @@
 #!/bin/sh -e
 if [ $(id -u) -ne 0 ]; then
-  podman unshare $0
+  podman unshare $0 $*
   exit
 fi
-IMAGE='localhost/rinse'
-[ -n "$1" ] && IMAGE=$1
-WORKDIR=$(pwd)
-mkdir -p $WORKDIR/rootfs
-cp -rp $(podman image mount $IMAGE)/* $WORKDIR/rootfs/
-podman image unmount $IMAGE > /dev/null
-echo To remove $WORKDIR/rootfs: podman unshare rm -rf $WORKDIR/rootfs/
+
+IMAGE="localhost/rinse"
+if [ -n "$1" ]; then
+  IMAGE=$1
+fi
+
+SRCDIR=$(podman image mount $IMAGE)
+if [ -n "$SRCDIR" ]; then
+  WORKDIR=$(pwd)
+  mkdir -p $WORKDIR/rootfs
+  cp -rp $SRCDIR/* $WORKDIR/rootfs/
+  podman image unmount $IMAGE > /dev/null
+  echo To remove $WORKDIR/rootfs: podman unshare rm -rf $WORKDIR/rootfs/
+fi
