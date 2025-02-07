9694e4afffc55cd89fef4fd65d3d9c8a4466135e
Johan Lindh <johan@linkdata.se>
2025-02-07 11:21:05 +0100

    get TIKA version

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ cd gvisor
 GO111MODULE=on go get gvisor.dev/gvisor/runsc@go
 CGO_ENABLED=0 GO111MODULE=on go build -o ../runsc gvisor.dev/gvisor/runsc
 cd ..
-podman build -t localhost/rinse .
+TIKAVERSION=$(curl -s https://dlcdn.apache.org/tika/ | grep -oE '3\.[0-9]+\.[0-9]+' | head -n 1)
+podman build --build-arg TIKAVERSION=$TIKAVERSION -t localhost/rinse .
 mkdir -p rootfs
 podman unshare ./copyrootfs.sh
 rm rinse rinse-devel runsc
